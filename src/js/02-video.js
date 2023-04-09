f9a14feb3f0bad36f28a7922a6cdbc6ddb825f5a
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-04-09 18:55:26 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,4 +14,15 @@ player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
   });
